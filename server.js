64d6c568859b5dab4bcfa7fda558edc3b99acfc7
taurusilver7 <prasad20697@gmail.com>
2021-04-22 23:54:09 +0530

    story route added to server

--- server.js
+++ server.js
@@ -48,6 +48,7 @@ app.use(express.static(path.join(__dirname, "public")));
 // api routes
 app.use("/", require("./routes/index"));
 app.use("/auth", require("./routes/auth"));
+app.use("/stories", require("./routes/stories"));
 
 // middlewares
 
