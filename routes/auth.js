9c5b7dc787ca3ffee92385a27a30a4ce75b64a77
taurusilver7 <prasad20697@gmail.com>
2021-04-22 16:18:26 +0530

    logout route added to auth

--- routes/auth.js
+++ routes/auth.js
@@ -22,4 +22,13 @@ router.get(
   }
 );
 
+/*
+@desc    Logout user
+@route  GET /auth/logout
+*/
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
+
 module.exports = router;
