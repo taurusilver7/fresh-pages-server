627697fbe5f1ad2bbae62b6dd567c5fd41b4e1ea
taurusilver7 <prasad20697@gmail.com>
2021-04-22 16:17:47 +0530

    controllers added to top-routes

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 // All routes in the top-level for the api are routed here [dashboard, home..,]
 const express = require("express");
 const router = express.Router();
+const { ensureGuest, ensureAuth } = require("../controller/auth");
 
 /*
 @desc    Login/landing page
 @route  GET /
 */
-router.get("/", (req, res) => {
+router.get("/", ensureGuest, (req, res) => {
   res.render("login", {
     layout: "login",
   });
@@ -16,7 +17,8 @@ router.get("/", (req, res) => {
 @desc    Dashboard
 @route  GET /dashboard
 */
-router.get("/dashboard", (req, res) => {
+router.get("/dashboard", ensureAuth, (req, res) => {
+  console.log(req.user);
   res.render("dashboard");
 });
 
