598bd07ea5d2ddede09dc48c8d95d591127f881f
taurusilver7 <prasad20697@gmail.com>
2021-04-22 22:10:51 +0530

    dashboard route rendered

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const router = express.Router();
 const { ensureGuest, ensureAuth } = require("../controller/auth");
 
+const Story = require("../models/Story");
+
 /*
 @desc    Login/landing page
 @route  GET /
@@ -17,9 +19,18 @@ router.get("/", ensureGuest, (req, res) => {
 @desc    Dashboard
 @route  GET /dashboard
 */
-router.get("/dashboard", ensureAuth, (req, res) => {
-  console.log(req.user);
-  res.render("dashboard");
+router.get("/dashboard", ensureAuth, async (req, res) => {
+  // console.log(req.user);
+  try {
+    const stories = await Story.find({ user: req.user.id }).lean();
+    res.render("dashboard", {
+      name: req.user.firstName,
+      stories,
+    });
+  } catch (err) {
+    console.error(err);
+    res.render("error/500");
+  }
 });
 
 module.exports = router;
