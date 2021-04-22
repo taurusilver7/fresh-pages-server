c956c21b19ad0f62f9b36852debbc55f4a3eef82
taurusilver7 <prasad20697@gmail.com>
2021-04-22 23:53:08 +0530

    stories add route created but not rendered

--- routes/stories.js
+++ routes/stories.js
@@ -1,4 +1,16 @@
 // Creating, editing stories routes are under stories.js
 const express = require("express");
 const router = express.Router();
-const { ensureGuest, ensureAuth } = require("../controller/auth");
+const { ensureAuth } = require("../controller/auth");
+
+const Story = require("../models/Story");
+
+/*
+@desc    Show add page
+@route  GET /stories/add
+*/
+router.get("/add", ensureAuth, (req, res) => {
+  res.render("stories/add");
+});
+
+module.exports = router;
