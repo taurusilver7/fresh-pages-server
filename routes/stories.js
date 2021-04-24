520f6b5420619606b6dfb3c07d78638d5fda95c9
taurusilver7 <prasad20697@gmail.com>
2021-04-24 11:16:26 +0530

    get pulic stories route rendered

--- routes/stories.js
+++ routes/stories.js
@@ -52,10 +52,15 @@ router.get("/", ensureAuth, async (req, res) => {
 router.get("/edit/:id", ensureAuth, async (req, res) => {
   const story = await Story.findOne({ _id: req.params.id }).lean();
 
-  if (!story) res.render("error/404");
+  if (!story) {
+    return res.render("error/404");
+  }
 
-  if (story.user !== req.user.id) res.redirect("/stories");
-  res.render("stories/edit", { story });
+  if (story.user != req.user.id) {
+    return res.redirect("/stories");
+  } else {
+    res.render("stories/edit", { story });
+  }
 });
 
 module.exports = router;
