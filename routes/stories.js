36942ce89e7d114c6ee02603550d3be188387118
taurusilver7 <prasad20697@gmail.com>
2021-04-24 11:39:56 +0530

    put req handle fn created

--- routes/stories.js
+++ routes/stories.js
@@ -63,4 +63,27 @@ router.get("/edit/:id", ensureAuth, async (req, res) => {
   }
 });
 
+/*
+@desc    Update story
+@route  PUT /stories/:id
+*/
+router.put("/:id", ensureAuth, async (req, res) => {
+  let story = await Story.findById(req.params.id).lean();
+
+  if (!story) {
+    return res.render("error/404");
+  }
+
+  if (story.user != req.user.id) {
+    return res.redirect("/stories");
+  } else {
+    story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.redirect("/dashboard");
+  }
+});
+
 module.exports = router;
