a70343b0ed3a4071f252cfc9446fbd529dfdacb3
taurusilver7 <prasad20697@gmail.com>
2021-04-24 12:02:07 +0530

    single story route created

--- routes/stories.js
+++ routes/stories.js
@@ -14,21 +14,6 @@ router.get("/add", ensureAuth, (req, res) => {
 });
 
 /*
-@desc    Process add form
-@route  POST /stories
-*/
-router.post("/", ensureAuth, async (req, res) => {
-  try {
-    req.body.user = req.user.id;
-    await Story.create(req.body);
-    res.redirect("/dashboard");
-  } catch (err) {
-    console.error(err);
-    res.render("/error/500");
-  }
-});
-
-/*
 @desc    Show all stories
 @route  GET /stories/
 */
@@ -46,20 +31,58 @@ router.get("/", ensureAuth, async (req, res) => {
 });
 
 /*
+@desc    Process add form
+@route  POST /stories
+*/
+router.post("/", ensureAuth, async (req, res) => {
+  try {
+    req.body.user = req.user.id;
+    await Story.create(req.body);
+    res.redirect("/dashboard");
+  } catch (err) {
+    console.error(err);
+    res.render("/error/500");
+  }
+});
+
+/*
+@desc    Show single story page
+@route  GET /stories/:id
+*/
+router.get("/:id", ensureAuth, async (req, res) => {
+  try {
+    let story = await Story.findById(req.params.id).populate("user").lean();
+
+    if (!story) {
+      return res.render("error/404");
+    }
+    res.render("stories/show", { story });
+  } catch (err) {
+    console.error(err);
+    res.render("error/404");
+  }
+});
+
+/*
 @desc    Show edit page
 @route  GET /stories/edit/:id
 */
 router.get("/edit/:id", ensureAuth, async (req, res) => {
-  const story = await Story.findOne({ _id: req.params.id }).lean();
+  try {
+    const story = await Story.findOne({ _id: req.params.id }).lean();
 
-  if (!story) {
-    return res.render("error/404");
-  }
+    if (!story) {
+      return res.render("error/404");
+    }
 
-  if (story.user != req.user.id) {
-    return res.redirect("/stories");
-  } else {
-    res.render("stories/edit", { story });
+    if (story.user != req.user.id) {
+      return res.redirect("/stories");
+    } else {
+      res.render("stories/edit", { story });
+    }
+  } catch (err) {
+    console.error(err);
+    res.render("error/500");
   }
 });
 
@@ -68,21 +91,40 @@ router.get("/edit/:id", ensureAuth, async (req, res) => {
 @route  PUT /stories/:id
 */
 router.put("/:id", ensureAuth, async (req, res) => {
-  let story = await Story.findById(req.params.id).lean();
+  try {
+    let story = await Story.findById(req.params.id).lean();
 
-  if (!story) {
-    return res.render("error/404");
+    if (!story) {
+      return res.render("error/404");
+    }
+
+    if (story.user != req.user.id) {
+      return res.redirect("/stories");
+    } else {
+      story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
+        new: true,
+        runValidators: true,
+      });
+
+      res.redirect("/dashboard");
+    }
+  } catch (error) {
+    console.error(err);
+    return res.render("error/500");
   }
+});
 
-  if (story.user != req.user.id) {
-    return res.redirect("/stories");
-  } else {
-    story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
-      new: true,
-      runValidators: true,
-    });
-
+/*
+@desc    Delete Story
+@route  DEL /stories/:id
+*/
+router.delete("/:id", ensureAuth, async (req, res) => {
+  try {
+    await Story.remove({ _id: req.params.id });
     res.redirect("/dashboard");
+  } catch (err) {
+    console.error(err);
+    return res.render("error/500");
   }
 });
 
