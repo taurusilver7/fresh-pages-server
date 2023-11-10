c8db1ca9e8f09ed47fd991702a9d96b8b14829bc
Devi prasad <prasad20697@gmail.com>
2023-11-10 10:26:44 +0530

    passport+db updated

--- config/db.js
+++ config/db.js
@@ -8,22 +8,23 @@ const DB = process.env.DATABASE_APPLICATON_URL.replace(
 */
 
 const options = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: true,
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useFindAndModify: true,
 };
 
 const connectDB = async () => {
-  try {
-    const connect = await mongoose.connect(
-      process.env.DATABASE_APPLICATON_URL,
-      options
-    );
-    console.log(`MongoDB Connected: ${connect.connection.host}`);
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
+	try {
+		const connect = mongoose.connect(
+			process.env.DATABASE_APPLICATON_URL,
+			options
+		);
+		console.log(`Server connected to Database!`);
+		// console.log(`MongoDB Connected: ${connect.connection.host}`);
+	} catch (err) {
+		console.error(err);
+		process.exit(1);
+	}
 };
 
 module.exports = connectDB;
