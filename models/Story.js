933738021774903f006f47a68032a1e46e6b12a2
Devi prasad <prasad20697@gmail.com>
2026-03-25 11:25:05 +0530

    story data model upgraded

--- models/Story.js
+++ models/Story.js
@@ -1,27 +1,52 @@
 const mongoose = require("mongoose");
 
+const ChapterSchema = new mongoose.Schema({
+	chapterNumber: {
+		type: Number,
+		required: true,
+	},
+	title: {
+		type: String,
+		default: function () {
+			return `Chapter ${this.chapterNumber}`;
+		},
+	},
+	content: {
+		type: String,
+		required: true,
+	},
+});
+
 const StorySchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  body: {
-    type: String,
-    required: true,
-  },
-  status: {
-    type: String,
-    default: "public",
-    enum: ["public", "private"],
-  },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+	title: {
+		type: String,
+		required: true,
+		trim: true,
+	},
+	description: {
+		type: String,
+		trim: true,
+	},
+	status: {
+		type: String,
+		default: "public",
+		enum: ["public", "private"],
+	},
+	chapters: {
+		type: [ChapterSchema],
+		validate: {
+			validator: (v) => v.length > 0,
+			message: "A story must have atleast one chapter.",
+		},
+	},
+	user: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "User",
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
 });
 
 const Story = mongoose.model("Story", StorySchema);
