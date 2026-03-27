774cb63701944a9331bb4ddadb33584b477277c1
Devi prasad <prasad20697@gmail.com>
2026-03-27 16:08:48 +0530

    handlerbar util

--- utils/hbs.js
+++ utils/hbs.js
@@ -3,44 +3,50 @@
 const moment = require("moment");
 
 module.exports = {
-  formatDate: function (date, format) {
-    return moment(date).format(format);
-  },
-  truncate: function (string, len) {
-    if (string.length > len && string.length > 0) {
-      let new_string = string + "";
-      new_string = string.substring(0, len);
-      new_string = string.substring(0, new_string.lastIndexOf(" "));
-      new_string =
-        new_string.length > 0 ? new_string : string.substring(0, len);
-      return new_string + "...";
-    }
-    return string;
-  },
-  stripTags: function (input) {
-    return input.replace(/<(?:.|\n)*?>/gm, "");
-  },
-  editIcon: function (storyUser, loggedUser, storyId, floating = true) {
-    if (storyUser._id.toString() == loggedUser._id.toString()) {
-      if (floating) {
-        return `<a href="/stories/edit/${storyId}" class="btn-floating halfway-fab blue"><i class="fas fa-edit fa-small"></i></a>`;
-      } else {
-        return `<a href="/stories/edit/${storyId}"><i class="fas fa-edit"></i></a>`;
-      }
-    } else {
-      return "";
-    }
-  },
-  select: function (selected, options) {
-    return options
-      .fn(this)
-      .replace(
-        new RegExp(' value="' + selected + '"'),
-        '$& selected="selected"'
-      )
-      .replace(
-        new RegExp(">" + selected + "</option>"),
-        ' selected="selected"$&'
-      );
-  },
+	formatDate: function (date, format) {
+		return moment(date).format(format);
+	},
+	truncate: function (str, len) {
+		if (!str) return "";
+		const string = str.toString();
+
+		if (string.length <= len) return string;
+
+		let truncated = string.substring(0, len);
+		truncated = truncated.substring(0, truncated.lastIndexOf(" "));
+		return (
+			(truncated.length > 0 ? truncated : string.substring(0, len)) + "..."
+		);
+	},
+	stripTags: function (input) {
+		if (!input) return "";
+		return input.toString().replace(/<(?:.|\n)*?>/gm, "");
+	},
+	editIcon: function (storyUser, loggedUser, storyId, floating = true) {
+		if (storyUser._id.toString() == loggedUser._id.toString()) {
+			if (floating) {
+				return `<a href="/stories/edit/${storyId}" class="btn-floating halfway-fab blue"><i class="fas fa-edit fa-small"></i></a>`;
+			} else {
+				return `<a href="/stories/edit/${storyId}"><i class="fas fa-edit"></i></a>`;
+			}
+		} else {
+			return "";
+		}
+	},
+	select: function (selected, options) {
+		return options
+			.fn(this)
+			.replace(
+				new RegExp(' value="' + selected + '"'),
+				'$& selected="selected"',
+			)
+			.replace(
+				new RegExp(">" + selected + "</option>"),
+				' selected="selected"$&',
+			);
+	},
+	json: function (context) {
+		return JSON.stringify(context);
+	},
+	
 };
